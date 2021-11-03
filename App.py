bdac325e56e81c0e1d76ae821f3b622d59c29bb6
SindaniBuk1 <79233513+SindaniBuk1@users.noreply.github.com>
2021-11-03 23:06:39 +0200

    Update App.py

--- App.py
+++ App.py
@@ -3,55 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
 import pandas as pd
+import montecarlo as mc
 from math import *
 from sympy import *
-#@st.cache
-
-def mc_moy(f,a,b,N):
-    x=np.random.uniform(a,b,N)
-    y=[]
-    for k in range(N):
-        y.append(f(x[k]))
-    df=(x,y)
-    return (b-a)*sum(y)/N,df
-def montecarlo(f,a,b,N):
-    n=0
-    S=(b-a)*np.max(f(np.linspace(a,b,1000000)))
-    x=np.random.uniform(a,b,N)
-    y=np.random.uniform(0,np.max(f(np.linspace(a,b,N))),N)
-    for i in range(N):
-        if (y[i]<=f(x[i])):
-            n=n+1
-    return S*n/N
-def mc_2d(f,N):
-    x_1,x_2=np.random.uniform(0,1,N),np.random.uniform(0,1,N)
-    z=[]
-    for k in range(N):
-        z.append(f(x_1[k],x_2[k]))
-    df=x_1,x_2,z
-    return sum(z)/N,df
-def mc_3d(f,N):
-    x_1,x_2,x_3=np.random.uniform(0,1,N),np.random.uniform(0,1,N),np.random.uniform(0,1,N)
-    z=[]
-    for k in range(N):
-        z.append(f(x_1[k],x_2[k],x_3[k]))
-    df=x_1,x_2,x_3,z
-    return sum(z)/N,df
-def mc_4d(f,N):
-    x_1,x_2,x_3,x_4=np.random.uniform(0,1,N),np.random.uniform(0,1,N),np.random.uniform(0,1,N),np.random.uniform(0,1,N)
-    z=[]
-    for k in range(N):
-        z.append(f(x_1[k],x_2[k],x_3[k],x_4[k]))
-    df=x_1,x_2,x_3,x_4,z
-    return sum(z)/N,df
-def mc_5d(f,N):
-    x_1,x_2,x_3,x_4,x_5=np.random.uniform(0,1,N),np.random.uniform(0,1,N),np.random.uniform(0,1,N),np.random.uniform(0,1,N),np.random.uniform(0,1,N)
-    z=[]
-    for k in range(N):
-        z.append(f(x_1[k],x_2[k],x_3[k],x_4[k],x_5[k]))
-    df=x_1,x_2,x_3,x_4,x_5,z
-    return sum(z)/N,df
-
+from mpl_toolkits.mplot3d import Axes3D
+#st.balloons()
 st.title("Calcul des intégrales par la méthode de montecarlo")
 st.sidebar.title("Opérations")
 dimension=st.sidebar.number_input("Dimension ",1)
@@ -76,7 +32,7 @@ if dimension==1:
 	f_s=sympify(fct)
 	
 	if st.button("Calculer","compute",help="Appuyez pour calculer l'intégrale"):
-		I,df=mc_moy(f,a,b,iterations)
+		I,df=mc.mc_moy(f,a,b,iterations)
 		txt="\int_{"+str(a)+"}^{"+str(b)+"}"+latex(sympify(fct.lower()))+"dx="+str(I)
 		st.latex(txt)
 		fig, ax = plt.subplots()
@@ -93,9 +49,11 @@ elif dimension==2:
 		txt="\iint ("+latex(sympify(fct.lower()))+")dxdy="+latex(integrate(f_s,x,y))
 		st.latex(txt)
 	if st.button("Calculer","compute",help="Appuyez pour calculer l'intégrale"):
-		I,df=mc_2d(f,iterations)
+		I,df=mc.mc_2d(f,iterations)
 		txt="\iint_{[0,1]^{ "+str(dimension)+"}}"+"("+latex(sympify(fct.lower()))+")dxdy="+str(I)
 		st.latex(txt)
+		fig = px.scatter_3d(df, x=df[0], y=df[1], z=f(df[0],df[1]))
+		st.write(fig)
 elif dimension==3:
 	fct3d="f=lambda x, y,z :"
 	fct=st.sidebar.text_input("f(x,y,z)",value="x")
@@ -107,7 +65,7 @@ elif dimension==3:
 		st.latex(txt)
 	iterations=st.sidebar.number_input("Nombre d'itérations",3,1000000,100)
 	if st.button("Calculer","compute",help="Appuyez pour calculer l'intégrale"):
-		I,df=mc_3d(f,iterations)
+		I,df=mc.mc_3d(f,iterations)
 		txt="\iiint_{[0,1]^{ "+str(dimension)+"}}"+"("+latex(sympify(fct.lower()))+")dxdydz="+str(I)
 		st.latex(txt)
 elif dimension==4:
@@ -117,12 +75,12 @@ elif dimension==4:
 	exec(fonction)
 	f_s=sympify(fct.lower())
 	if st.checkbox("Voir la primitive"):
-		txt="\iiint\int ("+latex(sympify(fct.lower()))+")dxdydzdt="+latex(integrate(f_s,x,y,z,t))
+		txt="\int ("+latex(sympify(fct.lower()))+")dxdydzdt="+latex(integrate(f_s,x,y,z,t))
 		st.latex(txt)
 	iterations=st.sidebar.number_input("Nombre d'itérations",3,1000000,100)
 	if st.button("Calculer","compute",help="Appuyez pour calculer l'intégrale"):
-		I,df=mc_4d(f,iterations)
-		txt="\int \iiint_{[0,1]^{ "+str(dimension)+"}}"+"("+latex(sympify(fct.lower()))+")dxdydzdt="+str(I)
+		I,df=mc.mc_4d(f,iterations)
+		txt="\int_{[0,1]^{ "+str(dimension)+"}}"+"("+latex(sympify(fct.lower()))+")dxdydzdt="+str(I)
 		st.latex(txt)
 elif dimension==5:
 	fct5d="f=lambda x, y,z,t,u :"
@@ -131,13 +89,15 @@ elif dimension==5:
 	exec(fonction)
 	f_s=sympify(fct)
 	if st.checkbox("Voir la primitive"):
-		txt="\iint\iiint("+latex(sympify(fct.lower()))+")dxdydzdtdu="+latex(integrate(f_s,x,y,z,t,u))
+		txt="\int("+latex(sympify(fct.lower()))+")dxdydzdtdu="+latex(integrate(f_s,x,y,z,t,u))
 		st.latex(txt)
 	iterations=st.sidebar.number_input("Nombre d'itérations",3,1000000,100)
 	if st.button("Calculer","compute",help="Appuyez pour calculer l'intégrale"):
-		I,df=mc_5d(f,iterations)
-		txt="\iint \iiint_{[0,1]^{ "+str(dimension)+"}}"+"("+latex(sympify(fct.lower()))+")dxdydzdtdu="+str(I)
+		I,df=mc.mc_5d(f,iterations)
+		txt="\int_{[0,1]^{ "+str(dimension)+"}}"+"("+latex(sympify(fct.lower()))+")dxdydzdtdu="+str(I)
 		st.latex(txt)
+
+
 
 		# x,y=df[0],df[1]
 		# z=f(x,y)
